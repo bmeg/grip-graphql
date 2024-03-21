eff73d3ee3cbe67512cbf4fcd0dd9bac998ed215
Kyle Ellrott <kellrott@gmail.com>
2024-03-21 14:44:41 -0700

    Adding command line arguments to proxy

--- cmd/grip-graphql-proxy/main.go
+++ cmd/grip-graphql-proxy/main.go
@@ -7,23 +7,37 @@ import (
 	"github.com/bmeg/grip/gripql"
 	"github.com/bmeg/grip/log"
 	"github.com/bmeg/grip/util/rpc"
-
 	"github.com/spf13/pflag"
 )
 
+var gripServer = "localhost:8202"
+var serverPort = "8080"
+var configFile = "config.js"
+var graph = "test-db"
+
 func main() {
+
+	//  <grip server> <server port> <config> <database>
+
+	pflag.StringVar(&gripServer, "grip", gripServer, "GRIP server")
+	pflag.StringVar(&serverPort, "port", serverPort, "Proxy port")
+	pflag.StringVar(&configFile, "config", configFile, "Config")
+	pflag.StringVar(&graph, "graph", graph, "Graph")
 
 	pflag.Parse()
 
-	client, err := gripql.Connect(rpc.ConfigWithDefaults("localhost:8202"), false)
+	client, err := gripql.Connect(rpc.ConfigWithDefaults(gripServer), false)
 	if err != nil {
 		log.Errorf("Error: %s", err)
 	}
 	config := map[string]string{}
 
+	config["graph"] = graph
+	config["config"] = configFile
+
 	httpHandler, err := gripgraphql.NewHTTPHandler(client, config)
 	if err != nil {
 		log.Errorf("Error: %s", err)
 	}
-	http.ListenAndServe(":8080", httpHandler)
+	http.ListenAndServe(":"+serverPort, httpHandler)
 }
