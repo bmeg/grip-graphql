1c40307ccbb433aadceb0d7f0be2164c91f0d5c1
matthewpeterkort <matthewpeterkort@gmail.com>
2024-03-29 16:09:46 -0700

    gen3 configs

---
+++ config/gen3.js
@@ -0,0 +1,115 @@
+endpoint.add({
+    name: "DocumentReference",
+    schema: [{
+        id: "String",
+        status: "String",
+        resourceType: "String",
+        category: [{
+            coding: [{
+                code: "String",
+                display: "String",
+                system: "String"
+            }]
+        }],
+        content: [{
+            attachment: {
+                url: "String"
+            }
+        }],
+        identifier: [{
+            system: "String",
+            value: "String"
+        }]
+    }],
+    args: {
+        offset: "Int",
+        limit: "Int",
+        project_id : "String"
+    },
+    defaults: {
+        offset: 0,
+        limit: 100
+    },
+    handler: (G, args) => {
+        return G.V().hasLabel("DocumentReference").skip(args.offset).limit(args.limit).toList()
+    }
+})
+
+// Returns all Patient Ids that have a docref edge connected to them.
+endpoint.add({
+    name: "PatientIdsWithDocumentEdge",
+    schema: [{
+        id: "String"
+    }],
+    args: {
+        offset: "Int",
+        limit: "Int",
+        project_id : "String"
+    },
+    defaults: {
+        offset: 0,
+        limit: 100
+    },
+    handler: (G, args) => {
+        return G.V().hasLabel("DocumentReference").outE("subject").out().skip(args.offset).limit(args.limit).toList()
+    }
+})
+
+endpoint.add({
+    name: "PatientIdsWithSpecimenEdge",
+    schema: [{
+        id: "String"
+    }],
+    args: {
+        offset: "Int",
+        limit: "Int",
+        project_id : "String"
+    },
+    defaults: {
+        offset: 0,
+        limit: 100
+    },
+    handler: (G, args) => {
+        return G.V().hasLabel("Specimen").outE().out().skip(args.offset).unique().limit(args.limit).toList()
+    }
+})
+
+endpoint.add({
+    name: "PatientIdsWithEncounterEdge",
+    schema: [{
+        id: "String"
+    }],
+    args: {
+        offset: "Int",
+        limit: "Int",
+        project_id : "String"
+    },
+    defaults: {
+        offset: 0,
+        limit: 100
+    },
+    handler: (G, args) => {
+        return G.V().hasLabel("Encounter").outE().out().skip(args.offset).limit(args.limit).toList()
+    }
+})
+
+// {"query":"{  PatientIdsWithDocumentEdge {    id  } }","variables":{}}
+// {"query":"query Query($limit: Int){ PatientIdsWithEncounterEdge(limit: $limit) { id }}","variables":{"limit":100000000},"operationName":"Query"}
+endpoint.add({
+    name: "PatientIdsWithObservationEdge",
+    schema: [{
+        id: "String"
+    }],
+    args: {
+        offset: "Int",
+        limit: "Int",
+        project_id : "String"
+    },
+    defaults: {
+        offset: 0,
+        limit: 100
+    },
+    handler: (G, args) => {
+        return G.V().hasLabel("Observation").outE().out().skip(args.offset).limit(args.limit).toList()
+    }
+})
